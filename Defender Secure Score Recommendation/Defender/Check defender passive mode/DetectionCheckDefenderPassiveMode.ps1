302a8123e3c64e4d1493133375c820983762a9bf
Magnus Brenno Mikkelsen <41649651+MikkelsenBrenno@users.noreply.github.com>
2026-04-21 12:43:09 +0200

    PassiveModeDefender
    This script checks if defender is in passive mode, if it is it tries to set it to active. 
    
    Typical things that have some left overs from a 3rd party software

---
+++ Defender Secure Score Recommendation/Defender/Check defender passive mode/DetectionCheckDefenderPassiveMode.ps1
@@ -0,0 +1,32 @@
+﻿$RegistryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection"
+$ValueName     = "ForceDefenderPassiveMode"
+$ExpectedValue = 0
+
+try {
+    if (-not (Test-Path -Path $RegistryPath)) {
+        Write-Output "Preferred value is set."
+        exit 0
+    }
+
+    $Item = Get-ItemProperty -Path $RegistryPath -ErrorAction Stop
+
+    if (-not ($Item.PSObject.Properties.Name -contains $ValueName)) {
+        Write-Output "Preferred value is set."
+        exit 0
+    }
+
+    $CurrentValue = $Item.$ValueName
+
+    if ($CurrentValue -eq $ExpectedValue) {
+        Write-Output "Preferred value is set."
+        exit 0
+    }
+    else {
+        Write-Output "Expected value is not set needs remidiation."
+        exit 1
+    }
+}
+catch {
+    Write-Output "Please follow up on the device. Unable to read value."
+    exit 1
+}
---
+++ Defender Secure Score Recommendation/Defender/Check defender passive mode/RemidiationSetDefenderToActive.ps1
@@ -0,0 +1,24 @@
+﻿$RegistryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection"
+$ValueName     = "ForceDefenderPassiveMode"
+$DesiredValue  = 0
+
+try {
+    if (Test-Path -Path $RegistryPath) {
+        $Item = Get-ItemProperty -Path $RegistryPath -ErrorAction Stop
+
+        if ($Item.PSObject.Properties.Name -contains $ValueName) {
+            New-ItemProperty -Path $RegistryPath `
+                             -Name $ValueName `
+                             -Value $DesiredValue `
+                             -PropertyType DWord `
+                             -Force | Out-Null
+        }
+    }
+
+    Write-Output "Remediation successful."
+    exit 0
+}
+catch {
+    Write-Output "Remediation failed."
+    exit 1
+}
